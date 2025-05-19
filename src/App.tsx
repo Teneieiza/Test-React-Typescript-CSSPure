16147344ff685bb5bafd60c2b035fe5cb8ccb489
tenmonster <tenmonster@hotmail.com>
2025-05-19 16:59:16 +0700

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddUserForm from "./components/addUserForm";
+import DarkModeToggle from "./components/darkModeToggle";
+import UserList from "./components/userList";
+import "./styles.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Management Dashboard</h1>
+      <DarkModeToggle />
+      <AddUserForm />
+      <UserList />
     </div>
   );
-}
+};
 
 export default App;
